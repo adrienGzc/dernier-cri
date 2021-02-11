f125ce180d786f99b502cf2faf8cb9bdc2f991f1
Adrien Guezennec <adrien.guezennec@epitech.eu>
2021-02-11 01:55:48 +0100

    :bug: remove splash screen hideasync func

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -3,8 +3,6 @@ import React, { useEffect } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
 
-import * as SplashScreen from 'expo-splash-screen';
-
 import type { Dispatch } from '@dernierCri/services/store';
 
 interface AwesomeSplashProps extends DispatchProps {}
@@ -12,8 +10,6 @@ interface AwesomeSplashProps extends DispatchProps {}
 const AwesomeSplash = ({ setAppStatus }: AwesomeSplashProps) => {
   useEffect(() => {
     const startupAsync = async () => {
-      await SplashScreen.preventAutoHideAsync();
-      await SplashScreen.hideAsync();
       setAppStatus('LOADED');
     };
     startupAsync();
